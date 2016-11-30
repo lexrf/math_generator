b16b71cb121d2b517bb518dcb0c840b7218f94ce
lex <lexrf75@gmail.com>
2016-11-30 21:07:52 +0800

    add file in 11/30

--- test/manual_generator.py
+++ test/manual_generator.py
@@ -33,8 +33,52 @@ def plus_in_20_a():
     b = int(random.uniform(0, 20 - a))
     print a, "+ ", b, " = (   )"
 
+
+def continue_plus_in_20_a():
+    a = int(random.uniform(0, 20))
+    b = int(random.uniform(0, 20-a))
+    c = int(random.uniform(0, 20-a-b))
+    print a, "+", b, "+", c, "=(  )"
+
+
+def continue_plus_in_20_b():
+    a = int(random.uniform(0, 20))
+    b = int(random.uniform(0, 20-a))
+    c = int(random.uniform(20-a-b, 20))
+    print a, "+", b, "+", "(  )=",  c
+
+
+def continue_plus_in_20_c():
+    a = int(random.uniform(0, 20))
+    b = int(random.uniform(0, 20-a))
+    c = int(random.uniform(20-a-b, 20))
+    print "(  )+", a, "+", b, "=",  c
+
+
+def continue_plus_in_20_d():
+    a = int(random.uniform(0, 20))
+    b = int(random.uniform(0, 20-a))
+    c = int(random.uniform(20-a-b, 20))
+    print a, "+", "(  )+", b, "=",  c
+
+
+def continue_minus_in_20_a():
+    a = int(random.uniform(0, 20))
+    b = int(random.uniform(0, a))
+    c = int(random.uniform(0, a-b))
+    print a, "-", b, "-", c, "=(  )"
+
+
 if __name__ == '__main__':
-    l = [(plus_in_20_a, 20), (plus_in_20_b, 20), (plus_in_20_c, 20), (minus_in_20_a, 20), (minus_in_20_b, 20)]
+    l = [(minus_in_20_b, 10),
+         (minus_in_20_a, 5),
+         (plus_in_20_b, 5),
+         (plus_in_20_c, 5),
+         (continue_plus_in_20_a, 15),
+         (continue_plus_in_20_b, 15),
+         (continue_plus_in_20_c, 15),
+         (continue_plus_in_20_d, 15),
+         (continue_minus_in_20_a, 15)]
     for t, n in l:
         for x in xrange(1, n):
             t()
